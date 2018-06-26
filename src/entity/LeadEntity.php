9bc0eb5f3e0daf17a2b11c3d3dfc888c0a9d7e63
Владимир Самсонов <vs@Mac-mini-Administrator.local>
2018-06-26 16:11:18 +0300

    Добавлен сброс дополнительных заголовков для последующих запросов

--- src/entity/LeadEntity.php
+++ src/entity/LeadEntity.php
@@ -23,9 +23,15 @@ class LeadEntity extends BaseEntity
      * @param array $params дополнительные параметры запроса
      * @return mixed
      */
-    public function items($params = [])
+    public function items($params = [], \DateTime $updatedAfter = null)
     {
         $params = array_merge($this->auth->getCredentials(), $params);
+
+        if ($updatedAfter !== null) {
+            $this->client->addRequestHeaders([
+                'If-Modified-Since' => $updatedAfter->format('D, d M Y H:i:s')
+            ]);
+        }
 
         return $this->client->get(self::ACTION_NAME, $params);
     }
